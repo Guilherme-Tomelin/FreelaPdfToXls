f652c1597ba9458a43587dea11e9316183169a58
Guilherme-Tomelin <guilhermesantosgts@outlook.com>
2023-07-27 16:21:34 -0300

    Gerando a planilha completa

--- src/pdf_extractor.py
+++ src/pdf_extractor.py
@@ -4,10 +4,17 @@ import pandas as pd
 from pdfminer.high_level import extract_text
 
 class PDFExtractor:
+    
     def __init__(self, pdf_path):
         self.pdf_path = pdf_path
 
     def extract_text(self):
+        """
+        Extracts text content from a PDF file.
+
+        Returns:
+            str or None: The extracted text content from the PDF, or None if an error occurs.
+        """
         try:
             return extract_text(self.pdf_path)
         except Exception as e:
@@ -23,6 +30,13 @@ class InformationExtractor:
         return result.group(1) if result else None
 
     def extract_info(self):
+        """
+        Extracts information from a text using regular expression patterns.
+
+        Returns:
+            dict: A dictionary containing extracted information with keys as the information names.
+                If no information is found, an empty dictionary is returned.
+        """
         patterns = {
             "Número da NFS-e": r'Número da NFS-e\s*(\d+)',
             "Data Fato Gerador": r'Data Fato Gerador\s*(\d{2}/\d{2}/\d{4})',
@@ -35,37 +49,57 @@ class InformationExtractor:
         return {field: self._search_pattern(pattern) for field, pattern in patterns.items()}
 
 class SpreadsheetCreator:
-    def __init__(self, sheet_data, output_file):
-        self.sheet_data = sheet_data
+    def __init__(self, sheet_data_list, output_file):
+        self.sheet_data_list = sheet_data_list
         self.output_file = output_file
 
     def create_spreadsheet(self):
         fields = ["Número da NFS-e", "Data Fato Gerador", "Valor Total", "ISSRF", "ISSQN", "Local de Incidência do ISS"]
 
-        df = pd.DataFrame([self.sheet_data], columns=fields)
-
         output_folder = os.path.join(".", "output")
         os.makedirs(output_folder, exist_ok=True)
 
         output_path = os.path.join(output_folder, self.output_file)
+
+        df = pd.DataFrame(self.sheet_data_list, columns=fields)
         df.to_excel(output_path, index=False)
 
+def get_valid_pdfs(pdf_folder_path):
+    pdf_name_pattern = r'NFSE_(\d+)_(\d+)_(\d+)\.pdf'
+    valid_pdfs = []
+    for filename in os.listdir(pdf_folder_path):
+        match = re.match(pdf_name_pattern, filename)
+        if match:
+            valid_pdfs.append(os.path.join(pdf_folder_path, filename))
+    return valid_pdfs
+
+
+
+
 def main():
-    pdf_path = "./exemplo/NFSE_38_75011_2_1.pdf"
+    pdfs_folder = "./pdfs"
+    valid_pdfs = get_valid_pdfs(pdfs_folder)
 
-    pdf_extractor = PDFExtractor(pdf_path)
-    text = pdf_extractor.extract_text()
+    pdfs_path_list = [os.path.join(pdfs_folder, filename) for filename in os.listdir(pdfs_folder)]
 
-    if text:
-        info_extractor = InformationExtractor(text)
-        data_patterns = info_extractor.extract_info()
+    all_data_patterns = []
+    for pdf_path in pdfs_path_list:
+        pdf_extractor = PDFExtractor(pdf_path)
+        text = pdf_extractor.extract_text()
 
-        spreadsheet_creator = SpreadsheetCreator(data_patterns, "teste.xlsx")
+        if text:
+            info_extractor = InformationExtractor(text)
+            data_patterns = info_extractor.extract_info()
+            all_data_patterns.append(data_patterns)
+        else:
+            print(f"Não foi possível extrair o texto do PDF: {pdf_path}")
+
+    if all_data_patterns:
+        spreadsheet_creator = SpreadsheetCreator(all_data_patterns, "dados_pdfs.xlsx")
         spreadsheet_creator.create_spreadsheet()
-
-        print("Dados extraídos e planilha criada com sucesso.")
+        print("Dados extraídos de todos os PDFs e planilha consolidada criada com sucesso.")
     else:
-        print("Não foi possível extrair o texto do PDF.")
+        print("Não foi possível extrair dados de nenhum PDF.")
 
 if __name__ == "__main__":
     main()
