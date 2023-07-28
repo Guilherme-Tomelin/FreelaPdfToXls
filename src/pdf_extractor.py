7108124e47e6eea44008d0068533b1d75776ef77
Guilherme-Tomelin <guilhermesantosgts@outlook.com>
2023-07-28 15:04:06 -0300

    Refatoração, pdfminer para PyPDF2, mudança no regex da Data Fato Gerador

--- src/pdf_extractor.py
+++ src/pdf_extractor.py
@@ -2,9 +2,9 @@ import os
 import re
 import pandas as pd
 from pdfminer.high_level import extract_text
+import PyPDF2
 
 class PDFExtractor:
-    
     def __init__(self, pdf_path):
         self.pdf_path = pdf_path
 
@@ -16,7 +16,12 @@ class PDFExtractor:
             str or None: The extracted text content from the PDF, or None if an error occurs.
         """
         try:
-            return extract_text(self.pdf_path)
+            with open(self.pdf_path, 'rb') as file:
+                pdf_reader = PyPDF2.PdfReader(file)
+                text = ""
+                for page in pdf_reader.pages:
+                    text += page.extract_text()
+            return text
         except Exception as e:
             print(f"Erro ao extrair o conteúdo do PDF: {e}")
             return None
@@ -26,6 +31,15 @@ class InformationExtractor:
         self.text = text
 
     def _search_pattern(self, pattern):
+        """
+        Search for a specific regular expression pattern in the given text.
+
+        Args:
+            pattern (str): Regular expression pattern to search for.
+
+        Returns:
+            str or None: The extracted pattern match or None if not found.
+        """
         result = re.search(pattern, self.text)
         return result.group(1) if result else None
 
@@ -43,7 +57,7 @@ class InformationExtractor:
 
         patterns = {
             "Número da NFS-e": r'Número da NFS-e\s*(\d+)',
-            "Data Fato Gerador": r'Data Fato Gerador\s*(\d{2}/\d{2}/\d{4})',
+            "Data Fato Gerador": r'Data/Hora Emissão\s*(\d{2}/\d{2}/\d{4})',
             "Valor Total": r'Valor Total\s*(\d{1,3}(?:\.\d{3})*(?:,\d{2}))',
             "ISSRF": r'ISSRF\s*(\d{1,3}(?:\.\d{3})*(?:,\d{2}))',
             "ISSQN": r'ISSQN\s*(\d{1,3}(?:\.\d{3})*(?:,\d{2}))',
@@ -68,6 +82,16 @@ class SpreadsheetCreator:
         self.output_file = output_file
 
     def create_spreadsheet(self):
+
+        """
+        Creates a spreadsheet (Excel file) from the provided data.
+
+        The spreadsheet will contain the extracted information.
+
+        Returns:
+            None
+        """
+
         fields = ["Número da NFS-e", "Data Fato Gerador", "Valor Total", "ISSRF", "ISSQN", "Local de Incidência do ISS"]
 
         output_folder = os.path.join(".", "output")
@@ -79,6 +103,19 @@ class SpreadsheetCreator:
         df.to_excel(output_path, index=False)
 
 def get_valid_pdfs(pdf_folder_path):
+
+    """
+    Get a list of valid PDF files in the specified folder.
+
+    PDF files are considered valid if they match a specific name pattern.
+
+    Args:
+        pdf_folder_path (str): The folder path containing the PDF files.
+
+    Returns:
+        list: A list of valid PDF file paths.
+    """
+
     pdf_name_pattern = r'NFSE_(\d+)_(\d+)_(\d+)\.pdf'
     valid_pdfs = []
     for filename in os.listdir(pdf_folder_path):
@@ -87,10 +124,16 @@ def get_valid_pdfs(pdf_folder_path):
             valid_pdfs.append(os.path.join(pdf_folder_path, filename))
     return valid_pdfs
 
-
-
-
 def main():
+
+    """
+    Main function to extract information from PDF files and create a spreadsheet.
+
+    This function uses the PDFExtractor and InformationExtractor classes to extract information
+    from each valid PDF file found in the specified folder. The extracted data is then used to create
+    a spreadsheet using the SpreadsheetCreator class.
+    """
+
     pdfs_folder = "./pdfs"
     valid_pdfs = get_valid_pdfs(pdfs_folder)
 
